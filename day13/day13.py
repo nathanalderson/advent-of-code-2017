4c338f8b9d0a52357b732dd7af2247eea97783fd
Nathan Alderson <nathan.alderson@gmail.com>
2017-12-17 13:40:00 -0600

    day13 - part2

--- day13/day13.py
+++ day13/day13.py
@@ -1,38 +1,25 @@
 #! /usr/bin/env python3
+import itertools
 
 def main():
     # depth -> (range, pos, direction)
-    initial_scanners = {
-        int(l[0]) : (int(l[1]), 0, "down") for l in (
+    scanners = {
+        int(l[0]) : int(l[1]) for l in (
             line.split(": ") for line in open("input", "r").readlines())
     }
-    severity = run_it(initial_scanners.copy())
+    severity = sum(pos * scanners[pos]
+                for pos in scanners
+                if scanner_pos(scanners[pos], pos) == 0)
     print("part1: severity =", severity)
+    delay = next(delay for delay in itertools.count()
+                 if not any(scanner_pos(scanners[pos], delay+pos) == 0
+                            for pos in scanners)
+                )
+    print("part2: delay =", delay)
 
-def run_it(scanners):
-    finish_line = 97
-    severity = 0
-    for d in range(finish_line):
-        scanner_range, scanner_pos, _ = scanners.get(d, (0, -1, "down"))
-        if scanner_pos == 0:
-            severity += d * scanner_range
-        advance_scanners(scanners)
-    return severity
-
-def advance_scanners(scanners):
-    for d, (r, p, dir) in scanners.items():
-        next_dir = dir
-        if dir == "down":
-            next_pos = p + 1
-            if next_pos == r:
-                next_pos = p - 1
-                next_dir = "up"
-        else:
-            next_pos = p - 1
-            if next_pos == -1:
-                next_pos = p + 1
-                next_dir = "down"
-        scanners[d] = (r, next_pos, next_dir)
+def scanner_pos(height, time):
+    positions = tuple(range(height)) + tuple(range((height-2), 0, -1))
+    return positions[time % len(positions)]
 
 if __name__ == "__main__":
     main()
