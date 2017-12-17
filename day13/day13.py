45b7c83406a3bcf5e07d8d20af9cf4af939fab9d
Nathan Alderson <nathan.alderson@gmail.com>
2017-12-17 13:59:40 -0600

    day13 - refactor and optimize

--- day13/day13.py
+++ day13/day13.py
@@ -2,23 +2,24 @@
 import itertools
 
 def main():
-    # depth -> (range, pos, direction)
-    scanners = {
-        int(l[0]) : int(l[1]) for l in (
-            line.split(": ") for line in open("input", "r").readlines())
-    }
-    severity = sum(pos * scanners[pos]
-                for pos in scanners
-                if scanner_pos(scanners[pos], pos) == 0)
+    lines = open("input", "r").readlines()
+    # [(depth, range)]
+    scanners = [
+        (int(d), int(r))
+        for d,r in (line.split(": ") for line in lines)
+    ]
+    severity = sum(d * r
+                   for d, r in scanners
+                   if scanner_pos(r, d) == 0)
     print("part1: severity =", severity)
     delay = next(delay for delay in itertools.count()
-                 if not any(scanner_pos(scanners[pos], delay+pos) == 0
-                            for pos in scanners)
+                 if not any(scanner_pos(r, delay+d) == 0
+                            for d, r in scanners)
                 )
     print("part2: delay =", delay)
 
-def scanner_pos(height, time):
-    positions = tuple(range(height)) + tuple(range((height-2), 0, -1))
+def scanner_pos(r, time):
+    positions = tuple(range(r)) + tuple(range((r-2), 0, -1))
     return positions[time % len(positions)]
 
 if __name__ == "__main__":
